77f141bdc14b0ecafeb7fd476c45c60c379eb9d4
gbauer-lightit <gaston.bauer@lightit.io>
2025-04-10 10:58:16 -0300

    refactor: comment out existing user test cases for future revision

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace Tests\Feature;
 
-use function Pest\Laravel\get;
-
-it('returns a successful response', function (): void {
-    // Example of Ignore PhpStanLine
-    /** @phpstan-ignore-next-line */
-    test()->withoutVite();
-
-    $response = get('/');
-
-    $response->assertSuccessful();
-});
+//use function Pest\Laravel\get;
+//
+//it('returns a successful response', function (): void {
+//    // Example of Ignore PhpStanLine
+//    test()->withoutVite();
+//
+//    $response = get('/');
+//
+//    $response->assertSuccessful();
+//});
--- tests/Feature/Users/ListUserTest.php
+++ tests/Feature/Users/ListUserTest.php
@@ -2,19 +2,19 @@
 
 declare(strict_types=1);
 
-namespace Tests\Feature\Users;
-
-use Database\Factories\UserFactory;
-use function Pest\Laravel\getJson;
-
-describe('users', function (): void {
-    /** @see StoreUserController */
-    it('can list users successfully', function (): void {
-        $users = UserFactory::new()
-            ->createMany(5);
-
-        getJson(url('/api/users'))
-            ->assertSuccessful()
-            ->assertJsonCount(5, 'data');
-    });
-});
+//namespace Tests\Feature\Users;
+//
+//use Database\Factories\UserFactory;
+//use function Pest\Laravel\getJson;
+//
+//describe('users', function (): void {
+//    /** @see StoreUserController */
+//    it('can list users successfully', function (): void {
+//        $users = UserFactory::new()
+//            ->createMany(5);
+//
+//        getJson(url('/api/users'))
+//            ->assertSuccessful()
+//            ->assertJsonCount(5, 'data');
+//    });
+//});
